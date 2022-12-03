1e0653e7d58a0620613e2605d06c811fe68ded2f
Will Fleming <wfleming77@gmail.com>
2022-12-03 14:14:14 -0500

    day 3 solutions

---
+++ src/day_three.clj
@@ -0,0 +1,49 @@
+(ns day-three
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]))
+
+(defn rucksacks
+  [line]
+  (let [mid (-> line count (/ 2))]
+    (split-at mid line)))
+
+(defn common-item-type [rucksacks]
+  (->> rucksacks
+       (map set)
+       (apply set/intersection)
+       (first)))
+
+;; Lowercase item types a through z have priorities 1 through 26.
+;; Uppercase item types A through Z have priorities 27 through 52.
+;; user> (int \A)
+;; 65
+;; user> (int \a)
+;; 97
+(defn letter->priority [c]
+  (let [i (int c)]
+    (if (>= i 97)
+      (- i 96)
+      (- i 38))))
+
+(defn part-one []
+  (with-open [rdr (-> "day3-input.txt" io/resource io/reader)]
+    (->> rdr
+         line-seq
+         (map #(map letter->priority %))
+         (map rucksacks)
+         (map common-item-type)
+         (apply +))))
+
+(defn part-two []
+  (with-open [rdr (-> "day3-input.txt" io/resource io/reader)]
+    (->> rdr
+         line-seq
+         (map #(map letter->priority %))
+         (partition 3)
+         (map common-item-type)
+         (apply +))))
+
+(comment
+  (part-one) ;; 8105
+  (part-two) ;; 2363
+  )
